dcff26bf05f06ba8111f6a48c1823d98b00d6cb9
saikirandaduvai1 <saikiran.daduvai1999@gmail.com>
2024-04-26 06:44:34 +0000

    coding solution

---
+++ index.js
@@ -0,0 +1,18 @@
+const element = (
+  //   Write your code here.
+  <div class='container'>
+    <h1 class='heading'>Super Over League</h1>
+    <div class='imageContainer'>
+      <img
+        src='https://assets.ccbp.in/frontend/react-js/rcb-img.png'
+        class='imagesize'
+      />
+      <img
+        src='https://assets.ccbp.in/frontend/react-js/csk-img.png'
+        class='imagesize'
+      />
+    </div>
+  </div>
+)
+
+ReactDOM.render(element, document.getElementById('root'))
